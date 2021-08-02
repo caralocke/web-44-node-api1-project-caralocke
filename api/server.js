19ef33a69fcf00db68fac1e3aa17c86deecb4876
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-02 13:29:14 -0400

    Successfully set up endpoint for getting all users.

--- api/server.js
+++ api/server.js
@@ -25,3 +25,16 @@ server.post('/api/users', (req, res) => {
             })
     }
 })
+
+// [GET]
+server.get('/api/users', (req, res) => {
+    Users.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'The users information could not be retrieved'
+            })
+        })
+})

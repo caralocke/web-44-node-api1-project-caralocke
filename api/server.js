49399d49f8222818809f5793b55ff2c20c4e031f
Cara M Locke <81659057+caralocke@users.noreply.github.com>
2021-08-02 13:21:15 -0400

    Succesffully completed setting up the endpoints for adding a new user.

--- api/server.js
+++ api/server.js
@@ -6,3 +6,22 @@ const server = express()
 server.use(express.json())
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
+
+// [POST]
+
+server.post('/api/users', (req, res) => {
+    if(!req.body.name || !req.body.bio) {
+        res.status(400).json({ message: 'Please provide name and bio for the user'})
+    } else {
+        const { name, bio } = req.body
+        Users.insert({ name, bio })
+            .then(user => {
+                res.status(201).json(user)
+            })
+            .catch(err => {
+                res.status(500).json({
+                    message: 'There was an error while saving the user to the database',
+                })
+            })
+    }
+})
